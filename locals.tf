12f9011372cbf0de884348cd167734ac5354bd97
jb <jbwlab@gmail.com>
2024-08-02 20:40:05 +0200

    test 12

--- locals.tf
+++ locals.tf
@@ -15,7 +15,7 @@ locals {
   subnet_new_bits               = [for size in values(var.subnets_and_sizes) : size - var.address_space_size]
   cidr_subnets                  = cidrsubnets(local.virtual_network_address_space, local.subnet_new_bits...)
 
-  skip_nsg = ["AzureBastionSubnet", "virtual_machines"]
+  #skip_nsg = ["AzureBastionSubnet", "virtual_machines"]
   subnets = { for key, value in var.subnets_and_sizes : key => {
     name             = key
     address_prefixes = [local.cidr_subnets[index(local.subnet_keys, key)]]
